9509bde92f15bd14632c4e0e274803c7a0828520
IkariShinji001 <duytran2003dev@gmail.com>
2025-05-23 22:41:42 +0700

    feat: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ThemeProvider } from "next-themes";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider value={defaultSystem}>
       <ThemeProvider attribute="class" disableTransitionOnChange>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </ThemeProvider>
     </ChakraProvider>
   </React.StrictMode>
